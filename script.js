41421d1cc0647f0d279349bd661b246c206bfc9e
pejhvaman <pejhvaman@gmail.com>
2024-07-18 15:55:44 +0330

    refactoring code: App class

--- script.js
+++ script.js
@@ -13,12 +13,19 @@ const inputElevation = document.querySelector('.form__input--elevation');
 const list = document.querySelector('.list');
 const listBtn = document.querySelector('.list-btn');
 
-if (navigator.geolocation) {
-  //async
-  let map,
-    mapEvent,
-    zoomLevel = 15;
-  const listBtnHandler = function () {
+class App {
+  #workouts = [];
+  #map;
+  #mapEvent;
+  #zoomLevel = 15;
+  constructor() {
+    this.#getPosition();
+    form.addEventListener('submit', this.#newWorkout.bind(this));
+    inputType.addEventListener('change', this.#toggleType);
+    listBtn.addEventListener('click', this.#handleBtn.bind(this));
+  }
+
+  #listBtnHandler() {
     if (list.classList.contains('list--hidden')) {
       listBtn.textContent = 'Show list';
       return true;
@@ -26,11 +33,12 @@ if (navigator.geolocation) {
       listBtn.textContent = 'Hide list';
       return false;
     }
-  };
+  }
+  #handleList(state) {
+    list.classList[state]('list--hidden');
+  }
 
-  //helper functions
-  const handleList = state => list.classList[state]('list--hidden');
-  const handleForm = state => {
+  #handleForm(state) {
     if (state === 'add') {
       form.style.display = 'none';
       form.classList[state]('hidden');
@@ -38,93 +46,95 @@ if (navigator.geolocation) {
     } else {
       form.classList[state]('hidden');
     }
-  };
+  }
 
-  //getting current position
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude, longitude } = position.coords;
-      //setting view of the map in current position(L=leaflet library)
-      map = L.map('map').setView([latitude, longitude], zoomLevel);
-
-      //adding layer to map. we also have ....org/{z}/...
-      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      //instead of eventlistener on the map
-      map.on('click', function (mapE) {
-        mapEvent = mapE;
-        //show form part
-        handleForm('remove');
-        //rendering workout form
-        handleList('remove');
-        setTimeout(() => inputDistance.focus(), 100); //good to ux
-        //check classes for show/hide list btn
-        listBtnHandler();
-      });
-
-      map.on('movestart', function () {
-        handleList('add');
-
-        handleForm('add');
-
-        listBtnHandler();
-      });
-
-      form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        //validating form inputs then show them on map
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: '250',
-              autoClose: false,
-              // closeButton: false,
-              closeOnClick: false,
-              closeOnEscapeKey: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('workout')
-          .openPopup();
-        //clearing inputs
-        inputDistance.value =
-          inputDuration.value =
-          inputCadence.value =
-          inputElevation.value =
-            '';
-        handleForm('add');
-        handleList('add');
-        listBtnHandler();
-      });
-
-      inputType.addEventListener('change', function () {
-        inputCadence
-          .closest('.form__row')
-          .classList.toggle('form__row--hidden');
-        inputElevation
-          .closest('.form__row')
-          .classList.toggle('form__row--hidden');
-        inputDistance.focus();
-      });
-
-      listBtn.addEventListener('click', function () {
-        if (listBtnHandler()) {
-          handleList('remove');
-          listBtnHandler();
-        } else {
-          handleList('add');
-          handleForm('add');
-          listBtnHandler();
+  #getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        this.#loadMap.bind(this),
+        function () {
+          alert("Couldn't get your position.");
         }
-      });
-    },
-    function () {
-      alert("Couldn't get your position.");
+      );
     }
-  );
+  }
+  #loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    //setting view of the map in current position(L=leaflet library)
+    this.#map = L.map('map').setView([latitude, longitude], this.#zoomLevel);
+
+    //adding layer to map. we also have ....org/{z}/...
+    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    //instead of eventlistener on the map
+    this.#map.on('click', this.#showForm.bind(this));
+    this.#map.on('movestart', this.#hideForm.bind(this));
+  }
+
+  #showForm(mapE) {
+    this.#mapEvent = mapE;
+    //show form part
+    this.#handleForm('remove');
+    //rendering workout form
+    this.#handleList('remove');
+    setTimeout(() => inputDistance.focus(), 100); //good to ux
+    //check classes for show/hide list btn
+    this.#listBtnHandler();
+  }
+
+  #hideForm() {
+    this.#handleList('add');
+    this.#handleForm('add');
+    this.#listBtnHandler();
+  }
+
+  #newWorkout(e) {
+    e.preventDefault();
+    //validating form inputs then show them on map
+    const { lat, lng } = this.#mapEvent.latlng;
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: '250',
+          autoClose: false,
+          // closeButton: false,
+          closeOnClick: false,
+          closeOnEscapeKey: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('workout')
+      .openPopup();
+    //clearing inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+    this.#handleForm('add');
+    this.#handleList('add');
+    this.#listBtnHandler();
+  }
+
+  #toggleType() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputDistance.focus();
+  }
+
+  #handleBtn() {
+    if (this.#listBtnHandler()) {
+      this.#handleList('remove');
+      this.#listBtnHandler();
+    } else {
+      this.#handleList('add');
+      this.#handleForm('add');
+      this.#listBtnHandler();
+    }
+  }
 }
+const app = new App();
